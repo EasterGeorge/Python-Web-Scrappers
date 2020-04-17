b1809a30f8c08d9bbd00c7e93dbb8c464ad6c436
Easter George <33330733+EasterGeorge@users.noreply.github.com>
2020-04-17 23:54:49 +0300

    Add files via upload

---
+++ jobswebscraper.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 15:16:39 2020
+
+@author: user
+
+
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Nairobi'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='ResultsContainer')
+
+#print(results.prettify())
+
+job_elems = results.find_all('section', class_='card-content')
+for job_elem in job_elems:
+     # Each job_elem is a new BeautifulSoup object.
+    # You can use the same methods on it as you did before.
+    
+   
+
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    
+    if None in (title_elem, company_elem, location_elem):
+        continue
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(location_elem.text.strip())
+    print()
+
+
+"""
+python_jobs = results.find_all('h2',
+                               string=lambda text: "python" in text.lower())
+
+for p_job in python_jobs:
+    link = p_job.find('a')['href']
+    print(p_job.text.strip())
+    print(f"Apply here: {link}\n")
+
+The filtered results will only show links to job opportunities that 
+include python in their title. You can use the same square-bracket notation 
+to extract other HTML attributes as well.
+ A common use case is to fetch the URL of a link, as you did above.
+"""
